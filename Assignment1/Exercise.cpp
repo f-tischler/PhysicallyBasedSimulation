abf3bfb04527ede192bd99585358fac32d613c12
Florian Tischler <florian.tischler@outlook.com>
2017-10-13 15:35:02 +0200

    implemented Euler method made first point static added gravity added random forces when toggling user forces on (press "f")

--- Assignment1/Exercise.cpp
+++ Assignment1/Exercise.cpp
@@ -36,19 +36,80 @@ using namespace std;
 * This function is called every time step of the dynamic simulation.
 * Positions, velocities, etc. should be updated to simulate motion
 * of the mass points of the 2D scene. The selected solver is passed
-* to the function as well as the time step, the spings, and the 
+* to the function as well as the time step, the springs, and the 
 * mass points. 
 *
 *******************************************************************/
 
-void TimeStep(double dt, Scene::Method method, 
-              vector<Point> &points, vector<Spring> &springs, bool interaction)
+void euler(const double dt, 
+		   vector<Point> &points,
+		   vector<Spring> &springs,
+           const bool interaction)
+{
+	for(auto& point : points)
+	{
+		if(&point == &points.front())
+			continue;
+
+		point.setUserForce(Vec2(0, -10));
+
+		if(interaction)
+		{
+			point.setUserForce(point.getUserForce() + Vec2(
+				rand() % 50 * (rand() % 2 ? -1 : 1),
+				rand() % 50));
+		}
+
+		point.setPos(point.getPos() + point.getVel() * dt);
+
+		point.setForce(Vec2(0, 0));
+
+		for(auto& spring : springs)
+		{
+			if(spring.getPoint(0) != &point &&
+			   spring.getPoint(1) != &point)
+			{
+				continue;
+			}
+
+			const auto spring_point = spring.getPoint(0) == &point
+				? spring.getPoint(1)
+				: spring.getPoint(0);
+
+			const auto connection =
+				point.getPos() - spring_point->getPos();
+
+			const auto distance = connection.length();
+
+			if (abs(distance) < 0.00000001)
+				continue;;
+
+			const auto direction = connection.normalize();
+
+			const auto f = spring.getStiffness() *
+				(spring.getRestLength() - distance) * direction;
+
+			point.addForce(f);
+		}
+		
+		point.addForce(point.getUserForce());
+		
+		const auto a = (point.getForce() - point.getDamping() * point.getVel()) /
+			point.getMass();
+
+		point.setVel(point.getVel() + a * dt);
+	}
+
+}
+
+void TimeStep(const double dt, const Scene::Method method, 
+              vector<Point> &points, vector<Spring> &springs, const bool interaction)
 { 
     switch (method)
     {
         case Scene::EULER:
         {
-             break;
+			return euler(dt, points, springs, interaction);
         }
         
         case Scene::SYMPLECTIC: 
