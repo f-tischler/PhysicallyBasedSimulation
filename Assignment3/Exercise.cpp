f12bfa9734de157ffb37d275a15b8336d7e7a6a3
Ivan Waldboth <ivan@waldboth.com>
2018-01-09 19:03:35 +0100

    still not working

--- Assignment3/Exercise.cpp
+++ Assignment3/Exercise.cpp
@@ -27,15 +27,18 @@
 
 
 static int xRes_static = -1;
+constexpr double h=3.0f;
+constexpr double h2=9.0f;
 
-int index(Vector2 pos)
+
+int index(int x, int y)
 {
-    return pos.y() * xRes_static + pos.x();
+    return y * xRes_static + x;
 }
 
-void set_point(double* field, Vector2 pos,double value)
+void set_point(double* field, int x, int y,double value)
 {
-    int i=index(pos);
+    int i=index(x, y);
     field[i] = value;
 }
 
@@ -56,13 +59,16 @@ double sampleTrilinear(double *field, double x, double y) {
     double xFloatPart = fastModf(x,xIntPart);
     double yFloatPart = fastModf(y,yIntPart);
 
+    xIntPart=floor(xIntPart);
+    yIntPart=floor(yIntPart);
+
     double tmp1{0.0f}, tmp2{0.0f}, tmp3{0.0f}, tmp4{0.0f},
             tmp12{0.0f},tmp34{0.0f};
 
-    tmp1 = field[ index( {(int)xIntPart, (int)yIntPart} ) ];
-    tmp2 = field[ index( {(int)xIntPart + 1, (int)yIntPart} ) ];
-    tmp3 = field[ index( {(int)xIntPart, (int)yIntPart + 1} ) ];
-    tmp4 = field[ index( {(int)xIntPart + 1, (int)yIntPart + 1} ) ];
+    tmp1 = field[ index( xIntPart, yIntPart ) ];
+    tmp2 = field[ index( xIntPart + 1, yIntPart ) ];
+    tmp3 = field[ index( xIntPart, yIntPart + 1 ) ];
+    tmp4 = field[ index( xIntPart + 1, yIntPart + 1 ) ];
 
 
     tmp12 = mix(tmp1, tmp2, xFloatPart);
@@ -77,15 +83,10 @@ void AdvectWithSemiLagrange(int xRes, int yRes, double dt,
 {
     if(xRes_static==-1)
         xRes_static=xRes;
-
     for (int y = 1; y < yRes - 1; ++y) {
         for (int x = 1; x < xRes - 1; ++x) {
-            Vector2 cur_cell(x, y);
-            double xComp = xVelocity[index(cur_cell)];
-            double yComp = yVelocity[index(cur_cell)];
-
-            double xOffset = x - (xComp * dt);
-            double yOffset = y - (yComp * dt);
+            double xOffset = x - (xVelocity[index(x,y)] * dt);
+            double yOffset = y - (yVelocity[index(x,y)] * dt);
 
             xOffset = std::min((double) (xRes - 2),
                                std::max((double) 1.0f, xOffset));
@@ -94,7 +95,7 @@ void AdvectWithSemiLagrange(int xRes, int yRes, double dt,
 
             double newValue = sampleTrilinear(field, xOffset, yOffset);
 
-            set_point(tempField, cur_cell, newValue);
+            set_point(tempField, x, y, newValue);
         }
     }
 
@@ -105,48 +106,32 @@ void SolvePoisson(int xRes, int yRes, int iterations, double accuracy,
                   double* pressure, double* divergence)
 {
     double *tempPressure=(double*)malloc(sizeof(double)*xRes*yRes);
-
     for (int y = 0; y < yRes*xRes; ++y)
-        tempPressure[y]=0.0f;
+        tempPressure[y]=pressure[y];
+    double error=0;
 
+    int iteration=0;
     while (true) {
-        double error=0;
-        for (int y = 0; y < yRes; ++y) {
-            for (int x = 0; x < xRes; ++x) {
-                Vector2 cur_cell({x,y});
-                double current = pressure[index(cur_cell)];
-                //Using five points as it is necessary to take the local value into account as well
-                double newValue = ( current +
-                                    pressure[index({x + 1, y})] +
-                                    pressure[index({x, y + 1})] +
-                                    pressure[index({x - 1, y})] +
-                                    pressure[index({x, y - 1})]
-                                  ) / 5.0f;
+        iteration+=1;
+        for (int y = 1; y < yRes - 1; ++y) {
+            for (int x = 1; x < xRes - 1; ++x) {
+                double newValue = ( h2 * divergence[index(x,y)] +
+                                    ( pressure[index(x + 1, y)] +
+                                      pressure[index(x, y + 1)] +
+                                      pressure[index(x - 1, y)] +
+                                      pressure[index(x, y - 1)]
+                                    )
+                                  ) / 4.0f;
 
-                set_point(tempPressure,cur_cell,newValue);
-                error += current - newValue > 0 ? current - newValue : newValue - current;
+                error += abs(pressure[x,y] - newValue);
+                set_point(pressure,x,y,newValue);
             }
         }
-        if(error < accuracy)
+
+
+        if( error < accuracy)
             break;
-        for (int y = 0; y < yRes; ++y) {
-            for (int x = 0; x < xRes; ++x) {
-                Vector2 cur_cell({x,y});
-                double current = pressure[index(cur_cell)];
-                //Using five points as it is necessary to take the local value into account as well
-                double newValue = ( current +
-                                    tempPressure[index({x + 1, y})] +
-                                    tempPressure[index({x, y + 1})] +
-                                    tempPressure[index({x - 1, y})] +
-                                    tempPressure[index({x, y - 1})] -
-                                    divergence[index({x,y})]
-                                  ) / 5.0f;
-
-                set_point(pressure,cur_cell,newValue);
-                error += current - newValue > 0 ? current - newValue : newValue - current;
-            }
-        }
-        if(error < accuracy)
+        if(iteration >= iterations)
             break;
     }
 }
@@ -155,63 +140,18 @@ void CorrectVelocities(int xRes, int yRes, double dt, const double* pressure,
                        double* xVelocity, double* yVelocity)
 {
 
-    double* xVelocityTemp = new double[yRes*xRes];
-    double* yVelocityTemp = new double[yRes*xRes];
-    for (int y = 0; y < yRes*xRes; ++y) {
-        xVelocityTemp[y] = xVelocity[y] * 0.9f;
-        yVelocityTemp[y] = yVelocity[y] * 0.9f;
-    }
+    for (int y = 1; y < yRes; ++y) {
+        for (int x = 1; x < xRes; ++x) {
+            double presxy=pressure[index(x,y)];
+            double presx1y=pressure[index(x-1,y)];
+            double presxy1=pressure[index(x,y-1)];
+            double presx=presxy-presx1y;
+            double presy=presxy-presxy1;
 
-    for (int y = 0; y < yRes; ++y) {
-        for (int x = 0; x < xRes; ++x) {
-            Vector2 cur_cell(x, y);
-
-            double xComp = xVelocity[index(cur_cell)];
-            double yComp = yVelocity[index(cur_cell)];
-
-            double xOffset = x + (xComp * dt);
-            double yOffset = y + (yComp * dt);
-
-            xOffset = std::min((double) (xRes - 2),
-                               std::max((double) 1.0f, xOffset));
-            yOffset = std::min((double) (yRes - 2),
-                               std::max((double) 1.0f, yOffset));
-
-            double xIntPart, yIntPart;
-
-            double xFloatPart = fastModf(xOffset,xIntPart);
-            double yFloatPart = fastModf(yOffset,yIntPart);
-
-            double xInverseFloatPart = 1 - xFloatPart;
-            double yInverseFloatPart = 1 - yFloatPart;
-
-            int *indexes = new int[4];
-            indexes[0] = index( {(int)xIntPart, (int)yIntPart} );
-            indexes[1] = index( {(int)xIntPart, (int)yIntPart + 1} );
-            indexes[2] = index( {(int)xIntPart + 1, (int)yIntPart} );
-            indexes[3] = index( {(int)xIntPart + 1, (int)yIntPart + 1} );
-
-            double *xValues = new double[4];
-            double *yValues = new double[4];
-            xValues[0] = ( xComp * ((xInverseFloatPart + yInverseFloatPart) / 2) );
-            yValues[0] = ( yComp * ((xInverseFloatPart + yInverseFloatPart) / 2) );
-            xValues[1] = ( xComp * ((xInverseFloatPart + yFloatPart) / 2) );
-            yValues[1] = ( yComp * ((xInverseFloatPart + yFloatPart) / 2) );
-            xValues[2] = ( xComp * ((xFloatPart + yInverseFloatPart) / 2) );
-            yValues[2] = ( yComp * ((xFloatPart + yInverseFloatPart) / 2) );
-            xValues[3] = ( xComp * ((xFloatPart + yFloatPart) / 2) );
-            yValues[3] = ( yComp * ((xFloatPart + yFloatPart) / 2) );
-
-            for (int i = 0; i < 4; ++i) {
-                xVelocityTemp[ indexes[i] ] =
-                        ( xVelocityTemp[ indexes[i] ] + xValues[i] ) / 2;
-                yVelocityTemp[ indexes[i] ] =
-                        ( yVelocityTemp[ indexes[i] ] + yValues[i] ) / 2;
-            }
-
-
+            double deltaxVel = ( (dt / 1.0f) * ( (1.0f / h) * presx));
+            double deltayVel = ( (dt / 1.0f) * ( (1.0f / h) * presy));
+            xVelocity[index(x,y)] = xVelocity[index(x,y)] - deltaxVel;
+            yVelocity[index(x,y)] = yVelocity[index(x,y)] - deltayVel;
         }
     }
-    xVelocity=xVelocityTemp;
-    yVelocity=yVelocityTemp;
 }
